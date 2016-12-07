7e7838faa3f03c1728b201d0e310f5afb93eaec0
Kevin O'Brien <kjrobrien@gmail.com>
2016-12-07 20:49:12 +0000

    Added sample config file

---
+++ config.sample.py
@@ -0,0 +1,13 @@
+CONFIG = {
+	"TradeKing" : {
+		"CONSUMER_KEY" : "",
+		"CONSUMER_SECRET" : "",
+		"OAUTH_TOKEN" : "",
+		"OAUTH_SECRET" : ""
+	},
+	"tickers" : [],
+	"refresh" : 10,
+	"scrollDelay": 0.02,
+	"fontFile" : "6x9.bdf",
+	"debug" : True
+}
